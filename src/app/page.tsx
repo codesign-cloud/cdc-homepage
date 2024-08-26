95535fd716f8ef1d1647bf50d18cabfb2d8993a5
thinkdj <think.dj@gmail.com>
2024-08-27 01:22:02 +0530

    Landing page: cdc home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,79 @@
 import React from 'react';
 
+interface FeatItem {
+  name: string;
+}
+
+const features: FeatItem[] = [
+  { name: "Next.js" },
+  { name: "Tailwind CSS" },
+  { name: "TypeScript" },
+  { name: "Default .gitignore / next.config" },
+  { name: "Deploy to GitHub Pages" },
+];
+
+const Feature: React.FC<FeatItem> = ({ name }) => (
+  <li className="flex items-center text-gray-900 mt-2">
+    <svg className="w-4 h-4 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" fill="transparent" viewBox="0 0 24 24" stroke="green">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.75" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+    <span className="ml-3 text-md font-bold font-pj">{name}</span>
+  </li>
+);
+
 export default function Page() {
   return (
     <div>
+
       <section className="py-12 bg-white sm:py-16 lg:py-20" style={{ minHeight: '100vh', width: '100%' }}>
-      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div className="px-8 py-10 overflow-hidden lg:px-24 md:py-20 bg-gray-50 rounded-3xl">
-          <div className="max-w-lg mx-auto text-center">
-            <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl xl:text-5xl font-pj">You’re ready to get started</h2>
-          </div>
 
-          <ul className="flex flex-col items-center justify-center mt-8 space-y-5 sm:mt-12 lg:mt-16 md:flex-row md:space-y-0 md:space-x-12">
-            <li className="flex items-center text-gray-900">
-              <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-              <span className="ml-3 text-lg font-bold font-pj"> Next.js </span>
-            </li>
 
-            <li className="flex items-center text-gray-900">
-              <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-              <span className="ml-3 text-lg font-bold font-pj"> Tailwind CSS </span>
-            </li>
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="px-8 py-10 overflow-hidden lg:px-24 md:py-20 bg-gray-50 rounded-3xl">
 
-            <li className="flex items-center text-gray-900">
-              <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-              </svg>
-              <span className="ml-3 text-lg font-bold font-pj"> TypeScript </span>
-            </li>
-          </ul>
+            {/* Header */}
+            <div className="px-0 mx-auto max-w-7xl">
+              <div className="max-w-md">
+                <h2 className="text-3xl font-bold sm:text-4xl bg-gradient-to-r from-blue-400 to-violet-950 text-transparent bg-clip-text">
+                  cdc homepage.
+                </h2>
+                <p className="mt-4 text-sm font-normal leading-6 text-gray-500">
+                  Get started with a static site using Next.js, Tailwind CSS, and TypeScript. Deploy to GitHub Pages using GitHub Actions.
+                </p>
 
-          <div className="mt-8 text-center sm:mt-12">
-            <div className="relative inline-flex group">
-              <div
-                className="absolute duration-1000 rotate-180 transitiona-all opacity-70 -inset-px rounded-xl blur-lg filter group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200"
-                style={{ background: "linear-gradient(90deg, #44ff9a -0.55%, #44b0ff 22.86%, #8b44ff 48.36%, #ff6644 73.33%, #ebff70 99.34%)" }}
-              ></div>
+                <div className="mt-4">
+                  <a href="https://github.com/codesign-cloud/cdc-homepage" title="CDC Homepage" className="inline-flex items-center text-base font-medium text-gray-900 group">
+                    View repo
+                    <svg className="w-5 h-5 ml-2 transition-all duration-200 transform group-hover:translate-x-1"
+                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                      stroke-width="2">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+                    </svg>
+                  </a>
+                </div>
 
-              <a
-                href="#"
-                title=""
-                className="relative inline-flex items-center justify-center py-3.5 text-base font-bold text-white transition-all duration-200 bg-gray-900 border border-transparent px-9 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 font-pj hover:bg-opacity-90 rounded-xl"
-                role="button"
-              >
-                Start building
-              </a>
+              </div>
             </div>
 
-            <p className="mt-5 text-sm font-normal text-gray-500 font-pj">something awesome</p>
+
+            <ul className="flex flex-col flex-wrap mt-8">
+              {features.map((item, index) => (
+                <Feature key={index} {...item} />
+              ))}
+            </ul>
+
+            <div className="mt-8 text-center sm:mt-12">
+              <div className="relative inline-flex group">
+                <div
+                  className="absolute duration-1000 rotate-180 transitiona-all opacity-70 -inset-px rounded-xl blur-lg filter group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200"
+                  style={{ background: "linear-gradient(90deg, #44ff9a -0.55%, #44b0ff 22.86%, #8b44ff 48.36%, #ff6644 73.33%, #ebff70 99.34%)" }}
+                ></div>
+              </div>
+
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
     </div>
   );
 }
