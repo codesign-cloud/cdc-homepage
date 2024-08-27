1059da4538576f2b8e0c35f69e14e1d2ea66f2c6
thinkdj <think.dj@gmail.com>
2024-08-27 13:44:23 +0530

    script: serve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import CdcHomePage from '@/app/cdc/CdcHomepage';
 export default function Page() {
   return (
     <>
+      {/* Edit this page to get started */}
       <CdcHomePage />
     </>
   );
